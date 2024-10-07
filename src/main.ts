f294670be8aa18136c1c8043336a9c43654a3350
Norman Zhu <nzhu@ucsc.edu>
2024-10-07 15:11:10 -0700

    Step 3 complete

--- src/main.ts
+++ src/main.ts
@@ -9,19 +9,27 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-function quack() {
+function click_quack() {
   quackCounter++;
   divtext.innerHTML = `Quack! ${quackCounter} quacks.`;
+  // sound effect quack
+  // const audio = new Audio("quack.mp3");
+}
+
+function quack_interval(){
+    quackCounter++;
+    divtext.innerHTML = `Quack! ${quackCounter} quacks.`;
 }
 
 const divtext = document.createElement("div");
 divtext.innerHTML = "Click the duck!";
 app.append(divtext);
 
-let quackCounter : number = 0;
+let quackCounter: number = 0;
 
 const button = document.createElement("button");
 button.innerHTML = "🦆";
-button.addEventListener("click", quack);
+button.addEventListener("click", click_quack);
 app.append(button);
 
+setInterval(quack_interval, 1000);
