ecc30f12ff0167b509d6de8d95ce6d5170a08275
Norman Zhu <nzhu@ucsc.edu>
2024-10-07 14:37:51 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,7 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.innerHTML = "🦆";
+app.append(button);
