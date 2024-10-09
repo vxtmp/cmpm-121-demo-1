2606bc198df7801039daf8990e61f6cf7b0a6a4f
Norman Zhu <nzhu@ucsc.edu>
2024-10-09 11:52:48 -0700

    Step 7 complete

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,10 @@ const buyLv3QuackerButton = document.createElement("button"); // fourth button. 
 const LV1_INCREMENT = 0.1;
 const LV2_INCREMENT = 2.0;
 const LV3_INCREMENT = 50.0;
+let lv1_price = 10;
+let lv2_price = 100;
+let lv3_price = 1000;
+const PRICE_INCREMENT = 1.15;
 let quackCounter: number = 0;
 let lv1Counter: number = 0;
 let lv2Counter: number = 0;
@@ -28,7 +32,7 @@ let lastTime: number;
 document.title = gameName;
 header.innerHTML = gameName;
 app.append(header);
-divtext.innerHTML = "Click the duck!";  // Prompt and quack counter.
+divtext.innerHTML = "Click the duck!"; // Prompt and quack counter.
 app.append(divtext);
 // append a new line using typescript
 app.append(document.createElement("br"));
@@ -37,72 +41,82 @@ button.addEventListener("click", click_quack);
 app.append(button);
 
 app.append(document.createElement("br"));
-buyLv1QuackerButton.innerHTML = "Buy AutoQuacker: 10 quacks";
+buyLv1QuackerButton.innerHTML = `Buy AutoQuacker: ${lv1_price} quacks`;
 buyLv1QuackerButton.addEventListener("click", buy_one_quacker);
 app.append(buyLv1QuackerButton);
 app.append(lv1text);
 
 app.append(document.createElement("br"));
-buyLv2QuackerButton.innerHTML = "Buy Lv2 AutoQuacker: 100 quacks";
+buyLv2QuackerButton.innerHTML = `Buy Lv2 AutoQuacker: ${lv2_price} quacks`;
 buyLv2QuackerButton.addEventListener("click", buy_two_quacker);
 app.append(buyLv2QuackerButton);
 app.append(lv2text);
 
 app.append(document.createElement("br"));
-buyLv3QuackerButton.innerHTML = "Buy Lv3 AutoQuacker: 1000 quacks";
+buyLv3QuackerButton.innerHTML = `Buy Lv3 AutoQuacker: ${lv3_price} quacks`;
 buyLv3QuackerButton.addEventListener("click", buy_three_quacker);
 app.append(buyLv3QuackerButton);
 app.append(lv3text);
 
-function update_status(){
-    // truncate quackCounter to a whole integer
-    const rounded_down_quacks = Math.floor(quackCounter);
-    divtext.innerHTML = `Quack! ${rounded_down_quacks} quacks.`;
-    lv1text.innerHTML = `Lv1 Quackers: ${lv1Counter}`;
-    lv2text.innerHTML = `Lv2 Quackers: ${lv2Counter}`;
-    lv3text.innerHTML = `Lv3 Quackers: ${lv3Counter}`;
+function update_status() {
+  // truncate quackCounter to a whole integer
+  const rounded_down_quacks = Math.floor(quackCounter);
+  divtext.innerHTML = `Quack! ${rounded_down_quacks} quacks.`;
+  lv1text.innerHTML = `Lv1 Quackers: ${lv1Counter}`;
+  lv2text.innerHTML = `Lv2 Quackers: ${lv2Counter}`;
+  lv3text.innerHTML = `Lv3 Quackers: ${lv3Counter}`;
+  buyLv1QuackerButton.innerHTML = `Buy AutoQuacker: ${lv1_price} quacks`;
+  buyLv2QuackerButton.innerHTML = `Buy Lv2 AutoQuacker: ${lv2_price} quacks`;
+  buyLv3QuackerButton.innerHTML = `Buy Lv3 AutoQuacker: ${lv3_price} quacks`;
+
 }
 
 function click_quack() {
   quackCounter++;
-//   divtext.innerHTML = `Quack! ${quackCounter} quacks.
+  //   divtext.innerHTML = `Quack! ${quackCounter} quacks.
   update_status();
   // sound effect quack
   // const audio = new Audio("quack.mp3");
 }
 
 function buy_one_quacker() {
-  if (quackCounter >= 10) {
-    quackCounter -= 10;
+  if (quackCounter >= lv1_price) {
+    quackCounter -= lv1_price;
     // auto_quacker_coefficient += LV1_INCREMENT;
     lv1Counter++;
+    lv1_price *= PRICE_INCREMENT;
     update_status();
   }
 }
 
-function buy_two_quacker(){
-    if (quackCounter >= 100) {
-        quackCounter -= 100;
-        // auto_quacker_coefficient += LV2_INCREMENT;
-        lv2Counter++;   
-        update_status();
-    }
+function buy_two_quacker() {
+  if (quackCounter >= lv2_price) {
+    quackCounter -= lv2_price;
+    // auto_quacker_coefficient += LV2_INCREMENT;
+    lv2Counter++;
+    lv2_price *= PRICE_INCREMENT;
+    update_status();
+  }
 }
 
-function buy_three_quacker(){
-    if (quackCounter >= 1000) {
-        quackCounter -= 1000;
-        // auto_quacker_coefficient += LV3_INCREMENT;
-        lv3Counter++;
-        update_status();
-    }
+function buy_three_quacker() {
+  if (quackCounter >= lv3_price) {
+    quackCounter -= lv3_price;
+    // auto_quacker_coefficient += LV3_INCREMENT;
+    lv3Counter++;
+    lv3_price *= PRICE_INCREMENT;
+    update_status();
+  }
 }
 
 function framebasedIncrement(timestamp: number) {
   const deltaSeconds = (timestamp - (lastTime ?? timestamp)) / 1000;
   lastTime = timestamp;
   // quackCounter += auto_quacker_coefficient * deltaSeconds;
-  const increment = lv1Counter * LV1_INCREMENT + lv2Counter * LV2_INCREMENT + lv3Counter * LV3_INCREMENT;
+  const increment =
+    lv1Counter * LV1_INCREMENT +
+    lv2Counter * LV2_INCREMENT +
+    lv3Counter * LV3_INCREMENT;
   quackCounter += increment * deltaSeconds;
 
   update_status();
