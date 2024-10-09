b3819358a6ab5798b7cea29e7d4abbe90c1b3603
Norman Zhu <nzhu@ucsc.edu>
2024-10-09 11:07:38 -0700

    Step 5 complete

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,29 @@
 import "./style.css";
 
-const app: HTMLDivElement = document.querySelector("#app")!;
 
+// Globals.
+const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Super awesome amazing awesome game of awesomeness";
 const header = document.createElement("h1");
 const divtext = document.createElement("div");
-const button = document.createElement("button");
+const button = document.createElement("button"); // first button. click to quack.
+const buyQuackerButton = document.createElement("button"); // second button. buy quacker.
 let quackCounter: number = 0;
 let lastTime: number;
+let auto_increment_by_one_times: number = 0;
 
+// Initialize UI elements.
 document.title = gameName;
 header.innerHTML = gameName;
 app.append(header);
-
 divtext.innerHTML = "Click the duck!";
 app.append(divtext);
 button.innerHTML = "🦆";
 button.addEventListener("click", click_quack);
 app.append(button);
+buyQuackerButton.innerHTML = "Buy AutoQuacker: 10 quacks";
+buyQuackerButton.addEventListener("click", buy_one_quacker);
+app.append(buyQuackerButton);
 
 function click_quack() {
   quackCounter++;
@@ -26,22 +32,21 @@ function click_quack() {
   // const audio = new Audio("quack.mp3");
 }
 
-// DEPRECATED. Use framebasedIncrement instead.
-// function quack_interval() {
-//   quackCounter++;
-//   divtext.innerHTML = `Quack! ${quackCounter} quacks.`;
-// }
+function buy_one_quacker(){
+    if (quackCounter >= 10){
+        quackCounter -= 10;
+        auto_increment_by_one_times++;
+        divtext.innerHTML = `Quack! ${quackCounter} quacks.`;
+    }
+}
 
 function framebasedIncrement(timestamp: number) {
-  const deltaTime = timestamp - (lastTime ?? timestamp);
+  const deltaSeconds = (timestamp - (lastTime ?? timestamp)) / 1000;
   lastTime = timestamp;
+  quackCounter += auto_increment_by_one_times * deltaSeconds;
 
-  quackCounter += deltaTime / 1000;
   divtext.innerHTML = `Quack! ${quackCounter} quacks.`;
   requestAnimationFrame(framebasedIncrement);
 }
 
-
-// DEPRECATED. Use framebasedIncrement instead.
-// setInterval(quack_interval, 1000);
 requestAnimationFrame(framebasedIncrement);
