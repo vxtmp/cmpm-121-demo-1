7ebfe1311985d11bf2c38621cb6e520b4d7636ab
Norman Zhu <nzhu@ucsc.edu>
2024-10-10 23:19:36 -0700

    Step 10 renames, content expansion

--- src/main.ts
+++ src/main.ts
@@ -6,21 +6,66 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Ducky Clicker";
 const header = document.createElement("h1");
 const divText = document.createElement("div"); // Current quacks.
+const quacksPS = document.createElement("div"); // Quacks per second.
+quacksPS.innerHTML = "Quacks per second: 0";
 // const quackAudio = new Audio(quackSound);
 
 interface Item {
-    name: string;
-    price: number;
-    increment: number;
-    count: number;
-    div: HTMLDivElement;
-    button: HTMLButtonElement;
+  name: string;
+  price: number;
+  increment: number;
+  count: number;
+  div: HTMLDivElement;
+  button: HTMLButtonElement;
+  description: string;
 }
 
-const availableItems : Item[] = [
-    {name: "AutoQuacker", price: 10, increment: 0.1, count: 0, div: document.createElement("div"), button: document.createElement("button")},
-    {name: "Lv2 AutoQuacker", price: 100, increment: 2.0, count: 0, div: document.createElement("div"), button: document.createElement("button")},  
-    {name: "Lv3 AutoQuacker", price: 1000, increment: 50.0, count: 0, div: document.createElement("div"), button: document.createElement("button")} 
+const availableItems: Item[] = [
+  {
+    name: "AutoQuacker",
+    price: 10,
+    increment: 0.1,
+    count: 0,
+    div: document.createElement("div"),
+    button: document.createElement("button"),
+    description: "Handheld toy duck. Automatically quacks for you.",
+  },
+  {
+    name: "Actual Duck",
+    price: 100,
+    increment: 2.0,
+    count: 0,
+    div: document.createElement("div"),
+    button: document.createElement("button"),
+    description: "Handheld to--- hey wait a second, this is a real duck!",
+  },
+  {
+    name: "Robot Ducky Fog Horn",
+    price: 1000,
+    increment: 50.0,
+    count: 0,
+    div: document.createElement("div"),
+    button: document.createElement("button"),
+    description: "Quacks so loud it can be heard for miles. Can be scheduled to quack at 2AM every day. You monster.",
+  },
+  {
+    name: "MegaMallard",
+    price: 10000,
+    increment: 1000.0,
+    count: 0,
+    div: document.createElement("div"),
+    button: document.createElement("button"),
+    description: "A giant duck that quacks so loud it can be heard from space. It's also a mallard, which is a type of duck.",
+  },
+  {
+    name: "Ducktastrophe",
+    price: 100000,
+    increment: 20000.0,
+    count: 0,
+    div: document.createElement("div"),
+    button: document.createElement("button"),
+    description: "A singularity of quacks. The universe will never be the same.",
+  }
 ];
 
 const button = document.createElement("button"); // main button. click to quack.
@@ -34,28 +79,32 @@ header.innerHTML = gameName;
 app.append(header);
 divText.innerHTML = "Click the duck!"; // Prompt and quack counter.
 app.append(divText);
+app.append(quacksPS);
 
 app.append(document.createElement("br"));
 button.innerHTML = "🦆";
 button.addEventListener("click", click_quack);
 app.append(button);
 
-function buyItem (item: Item) {
-    if (quackCounter >= item.price) {
-        quackCounter -= item.price;
-        item.count++;
-        item.price *= PRICE_INCREMENT;
-        update_status();
-    }
+function buyItem(item: Item) {
+  if (quackCounter >= item.price) {
+    quackCounter -= item.price;
+    item.count++;
+    item.price *= PRICE_INCREMENT;
+    update_status();
+  }
 }
 
 for (const item of availableItems) {
-    // const buyButton = document.createElement("button");
-    app.append(document.createElement("br"));
-    item.button.innerHTML = `Buy ${item.name}: ${item.price} quacks`;
-    item.button.addEventListener("click", () => buyItem(item));
-    app.append(item.button);
-    app.append(item.div);
+  // const buyButton = document.createElement("button");
+  app.append(document.createElement("br"));
+  item.button.innerHTML = `Buy ${item.name}: ${item.price} quacks`;
+  item.button.addEventListener("click", () => buyItem(item));
+  app.append(item.button);
+  const description = document.createElement("div");
+    description.innerHTML = item.description;
+    app.append(description);
+  app.append(item.div);
 }
 
 function update_status() {
@@ -63,16 +112,28 @@ function update_status() {
   const rounded_down_quacks = Math.floor(quackCounter);
   divText.innerHTML = `Quack! ${rounded_down_quacks} quacks.`;
   for (const item of availableItems) {
-        item.div.innerHTML = `${item.name}s owned: ${item.count}`;
-        item.button.innerHTML = `Buy ${item.name}: ${item.price} quacks`;
+    item.div.innerHTML = `${item.name}s owned: ${item.count}`;
+    item.button.innerHTML = `Buy ${item.name}: ${item.price} quacks`;
   }
+  let increment = 0;
+  for (const item of availableItems) {
+    increment += item.count * item.increment;
+  }
+  quacksPS.innerHTML = `Quacks per second: ${increment.toFixed(2)}`;
 }
-
 function click_quack() {
   quackCounter++;
   update_status();
-    // quackAudio.play();
+  // quackAudio.play();
 }
+
+// hover mouse over button to show description as floating text
+// button.addEventListener("mouseover", () => {
+//   const description = document.createElement("div");
+//   description.innerHTML = "Click the duck!";
+//   app.append(description);
+// });
+
 
 function framebasedIncrement(timestamp: number) {
   const deltaSeconds = (timestamp - (lastTime ?? timestamp)) / 1000;
@@ -80,11 +141,11 @@ function framebasedIncrement(timestamp: number) {
   // quackCounter += auto_quacker_coefficient * deltaSeconds;
   let increment = 0;
   for (const item of availableItems) {
-        increment += item.count * item.increment;
+    increment += item.count * item.increment;
   }
-    // lv1Counter * LV1_INCREMENT +
-    // lv2Counter * LV2_INCREMENT +
-    // lv3Counter * LV3_INCREMENT;
+  // lv1Counter * LV1_INCREMENT +
+  // lv2Counter * LV2_INCREMENT +
+  // lv3Counter * LV3_INCREMENT;
   quackCounter += increment * deltaSeconds;
 
   update_status();
